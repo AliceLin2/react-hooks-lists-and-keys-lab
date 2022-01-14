8be445d428f389425929f11c0ed71d5b254fb7f2
shu Lin <bbqlinshu@gmail.com>
2022-01-13 23:32:08 -0500

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElement = links.map((link)=>{
+    const hyperLink = '#'+link
+    return <a key={link} href={hyperLink}>{link}</a>
+  })
+
+  return <nav>{linkElement}</nav>;
 }
 
 export default NavBar;
